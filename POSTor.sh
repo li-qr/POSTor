2070a0f3e06226812912574e146c06f03fd0bb1f
mzzw <ailoveliquan@outlook.com>
2019-07-18 23:45:45 +0800

    config command

--- POSTor.sh
+++ POSTor.sh
@@ -1,2 +1,36 @@
 #!/bin/bash
 
+help () 
+{
+ echo -e 'usage:
+  POSTor config -e default-environment.sh'
+}
+
+config ()
+{
+	shift
+  case $1 in
+    -f | --default-env) echo "default" ;;
+    *) help ;;
+  esac
+}
+
+
+#main ##################
+if [ ! -n "$1" ]
+then
+	help
+else
+while [ -n "$1" ]
+do
+case $1 in
+	-h | --help) help ;;
+	config) 
+		config $@ 
+		shift $# ;;
+	*) help ;;
+esac
+shift
+done
+fi
+#main end##############
