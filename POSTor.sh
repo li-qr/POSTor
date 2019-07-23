1131a1ab67128d08468c34cd02ba4add2cffce74
quan.li <quan.li@ly.com>
2019-07-23 22:03:10 +0800

    file selector

--- POSTor.sh
+++ POSTor.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-scriptdir=
-echo $0
-echo $(basename $0)
-echo $(dirname $0)
+envdir="/enviroments"
+hisdir="/histories"
+requestdir="/requests"
+currdir=$(pwd)
+scriptdir=$(cd $(dirname $0);pwd)
+$(cd $currdir)
+
+
 
 function help
 {
@@ -19,12 +23,40 @@ function configuration
   esac
 }
 
-function deal
+function dealbasic
 {
-  
+	local var1=0
+	for var2 in $(find "$scriptdir$requestdir" -name $(echo $1 | sed -e 's/./*&/g' -e 's/$/&*/g'))
+	do
+		local files[$var1]=$(basename "$var2")
+		 (( var1++ ))
+	done
+  if [ ${#files[@]} -eq 0 ] ;then
+		echo "没有找到匹配请求"; exit 0
+	elif [ ${#files[@]} -eq 1 ] ;then
+		echo ${files[0]}
+	elif [ ${#files[@]} -ge 2 ];then
+		PS3="选择一个请求处理："
+		select option in ${files[*]}
+		do
+			if [ $REPLY -gt 0 ] 2>/dev/null ;then 
+  			if [ $REPLY -le ${#files[@]} ];then
+					echo ${files[$REPLY-1]}
+				 break;
+				fi
+			fi
+			echo "输入编号"
+		done
+	fi
 }
 
 #main ##################
+
+if [ -f "$scriptdir$envdir/default.sh" ]
+then
+  source "$scriptdir$envdir/default.sh"
+fi
+
 if [ ! -n "$1" ]
 then
 	help
@@ -37,7 +69,7 @@ case $1 in
 		configuration $@ 
 		shift $# ;;
 	*)
-	 deal $@
+	 dealbasic $@
     shift $#	;;
 esac
 shift
