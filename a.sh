8da208eac20e501b3dbf963ca0dde84bff48dec9
mzzw <ailoveliquan@outlook.com>
2019-07-25 22:59:25 +0800

    7/25

--- a.sh
+++ a.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-a="dd"
-var="echo sdf"
-if [ 2 -gt 1 ];then
-	var=$var$a
-fi
-$var
+#>&2 vim n.txt < 'EOF
+#echo "sdfsdf" | tee a.txt -a b.txt
+#echo "sdfdsfds" | tee 1>&/dev/null c.txt d.txt
+echo "nnn" | tee a.txt | tee -a b.txt 1>/dev/null
+
